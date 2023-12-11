a6d2f2ae01b31c09610c2f4a340e330aa8e8121a
YarkoMarko <Yarema1310@gmail.com>
2023-12-11 20:14:41 +0200

    First

--- main2.py
+++ main2.py
@@ -1,4 +1,12 @@
+import gzip
+import pickle
+
 list = [int(input("Enter element: ")) for _ in range(10)]
 
-print(list)
+with open("txt.pickle", "wb") as file:
+    pickle.dump(list, file)
 
+with open("txt.pickle", "rb") as file:
+    new_list = pickle.load(file)
+
+print(new_list)
